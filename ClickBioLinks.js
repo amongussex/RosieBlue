82806b4a589637b6dfd3d98309841dcbd4c1f96d
axxtura <106402707+atyanex@users.noreply.github.com>
2023-04-20 09:18:02 -0600

    Create ClickBioLinks.js

---
+++ ClickBioLinks.js
@@ -0,0 +1,43 @@
+import { findByNameAll, findByProps } from "@vendetta/metro";
+import { url as URLOpener } from "@vendetta/metro/common";
+import { after } from "@vendetta/patcher";
+
+const ActionShitter = findByProps("hideActionSheet");
+const ups = [];
+
+function walkReactTree(root: any, visit: (node: any) => void) {
+    if (!root) return;
+
+    visit(root);
+    if (!root?.props?.children) return;
+
+    if (Array.isArray(root.props.children)) {
+        for (const child of root.props.children) {
+            walkReactTree(child, visit);
+        }
+    } else {
+        walkReactTree(root.props.children, visit);
+    }
+}
+
+// WHY DOES DISCORD HAVE TWO OF THESE IM GONNA EXPLODE
+for (const UserProfile of findByNameAll("BioText", false)) {
+    const up = after("default", UserProfile, (args, res) => {
+        if (!res?.props?.children) return;
+
+        walkReactTree(res, node => {
+            if (node.props?.accessibilityRole === "link") {
+                const url = node.props.children?.[0];
+                if (typeof url !== "string") return;
+
+                node.props.onPress = () => {
+                    URLOpener.openURL(url);
+                    ActionShitter.hideActionSheet();
+                };
+            }
+        });
+    });
+    ups.push(up);
+}
+
+export const onUnload = () => ups.forEach(up => up());
